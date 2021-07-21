a337593ca99659c09551a435cf234c2fbef40ba9
박세영 <seyoungs22@naver.com>
2021-07-21 16:48:40 +0900

    S05P12C102-3 : Add token
    S05P12C102-3 : Add token

---
+++ backend/src/main/java/com/web/curation/login/service/JwtService.java
@@ -0,0 +1,11 @@
+package com.web.curation.login.service;
+
+import java.util.Map;
+
+public interface JwtService {
+	
+	<T> String create(String key, T data, String subject);
+	Map<String, Object> get(String key);
+	String getUserId();
+	boolean isUsable(String jwt);
+}
