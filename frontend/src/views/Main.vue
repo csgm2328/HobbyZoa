53674dfae6bc08009c034c7143c78317bde1bb2f
이혜은 <snflo16@naver.com>
2021-07-21 14:41:52 +0900

    S05P12C102-61: Update header page

--- frontend/src/views/Main.vue
+++ frontend/src/views/Main.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
     <Header/>
+    <router-link
+      :to="{name: 'FeedCreate'}"
+      style=" text-decoration: none;"
+    >
+      <v-btn
+        v-show="!hidden"
+        color="cyan accent-3"
+        fixed
+        bottom
+        right
+        fab
+      >
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </router-link>
   </div>
 </template>
 
 <script>
-  import Header from '../components/Header'
+  import Header from '@/components/Header'
 
   export default {
     name: 'Main',
