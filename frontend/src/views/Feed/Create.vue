920dc642bc18cfa0c9bf8bbbc7ca5a1f08ed2631
이혜은 <snflo16@naver.com>
2021-07-22 02:16:29 +0900

    S05P12C102-85: Update feed create page

--- frontend/src/views/Feed/Create.vue
+++ frontend/src/views/Feed/Create.vue
@@ -7,47 +7,47 @@
         justify="center"
         no-gutters
       >
-        <v-col cols="12" sm="6" class="mt-3 pa-2">
-          <v-container
+        <v-col cols="12" sm="8" class="mt-3">
+          <v-file-input
             style="height: 30vh;"
-            class="pa-2 d-flex justify-center align-center"
-            outlined
-            title
+            class="d-flex justify-center align-center"
+            v-if="unselected_picture"
+            accept="image/png, image/jpeg, image/bmp"
+            placeholder="Pick picture(s)"
+            prepend-icon="mdi-camera"
+            label="Select picture(s)"
+            v-model="files"
+            @change="addFiles"
+            multiple
           >
-            <v-file-input
-              accept="image/png, image/jpeg, image/bmp"
-              placeholder="Pick picture(s)"
-              prepend-icon="mdi-camera"
-              label="Pick picture(s)"
-              multiple
+          </v-file-input>
+          <v-carousel
+            v-else
+            hide-delimiters
+            style="height: 30vh;"
+          >
+            <v-carousel-item
+              v-for="(file,i) in files"
+              :key="i"
+              reverse-transition="fade-transition"
+              transition="fade-transition"
+              tile
             >
-              <template v-slot:selection="{ text }">
-                <v-chip
-                  small
-                  label
-                  color="primary"
-                >
-                  {{ text }}
-                </v-chip>
-              </template>
-            </v-file-input>
-          </v-container>
+              <v-img :src="url[i]" contain max-height="30vh" max-width="100vw"></v-img>
+            </v-carousel-item>
+          </v-carousel>
         </v-col>
-        <v-col cols="12" sm="6"  class="my-3 px-1">
-          <v-container
+        <v-col cols="12" sm="8"  class="my-3 px-1">
+          <v-textarea fluid
+            counter
             style="height: 30vh;"
-            class="pa-2"
-            outlined
-            title
-          >
-            <v-text-field
-              height="30vh"
-              label="내용을 입력하세요"
-              placeholder="내용을 입력하세요"
-              solo
-            ></v-text-field>
-          </v-container>
+            label="Text"
+            :rules="rules"
+            :value="value"
+            placeholder="내용을 입력해주세요."
+          ></v-textarea>
         </v-col>
+        
       </v-row>
     </v-container>
 
@@ -66,6 +66,7 @@
 </template>
 
 <script>
+
   import Header from '@/components/Header'
 
   export default {
@@ -73,7 +74,38 @@
     components: {
       Header,
     },
+    data() {
+      return {
+
+        files: [],
+        url: [],
+        unselected_picture: true,
+        items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+            },
+            {
+              src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+            },
+            {
+              src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+            },
+            {
+              src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+            },
+          ],
+      }
+    },
+    methods: {
+      addFiles() {
+        this.files.forEach(file => {
+          this.url.push(URL.createObjectURL(file))
+        });
+        this.unselected_picture = false
+      }
+    }
   }
+
 </script>
 
 <style scoped>
