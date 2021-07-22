c2355053ffe7a4c4d73e0c1e013189bd80f39d91
양준영 <wnsdud4197@naver.com>
2021-07-22 11:21:03 +0900

    S05P12C102-39: Add signup success page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Main from '../views/Main.vue'
-import Signup from '../views/Signup.vue'
-import Login from '../views/Login.vue'
-import FeedCreate from '../views/Feed/Create.vue'
+import Home from '@/views/Home.vue'
+import Main from '@/views/Main.vue'
+import Signup from '@/views/User/Signup.vue'
+import Signupsuccess from '@/views/User/Signupsuccess.vue'
+import Login from '@/views/User/Login.vue'
+import FeedCreate from '@/views/Feed/Create.vue'
 
 Vue.use(VueRouter)
 
@@ -23,6 +24,11 @@ const routes = [
     path: '/signup',
     name: 'Signup',
     component: Signup
+  },
+  {
+    path: '/signupsuccess',
+    name: 'Signupsuccess',
+    component: Signupsuccess
   },
   {
     path: '/login',
