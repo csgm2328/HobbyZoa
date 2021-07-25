8fcb664b71cc8c225810fe08df55387c2d807637
박세영 <seyoungs22@naver.com>
2021-07-25 23:19:38 +0900

    S05P12C102-43: Update auth user
    S05P12C102-43: Update auth user

--- backend/src/main/java/com/web/curation/user/repo/UserRepo.java
+++ backend/src/main/java/com/web/curation/user/repo/UserRepo.java
@@ -2,10 +2,14 @@ package com.web.curation.user.repo;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.web.curation.user.model.User;
 
 public interface UserRepo extends JpaRepository<User, String> {
 	User getUserByEmail(String email);
-
+//	@Query("from User u where user_id = :uid")
+//	User findUser(@Param("uid")String user_id);
     Optional<User> findUserByEmailAndPassword(String email, String password);
 }
