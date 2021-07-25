b15894fc99544e7d1c9f56346caf9a39e5ec6c20
박세영 <seyoungs22@naver.com>
2021-07-25 14:47:38 +0000

    Update backend/src/main/java/com/web/curation/user/repo/UserRepo.java

--- backend/src/main/java/com/web/curation/user/repo/UserRepo.java
+++ backend/src/main/java/com/web/curation/user/repo/UserRepo.java
@@ -2,10 +2,6 @@ package com.web.curation.user.repo;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< backend/src/main/java/com/web/curation/user/repo/UserRepo.java
-=======
->>>>>>> backend/src/main/java/com/web/curation/user/repo/UserRepo.java
-
 import com.web.curation.user.model.User;
 
 public interface UserRepo extends JpaRepository<User, String> {
