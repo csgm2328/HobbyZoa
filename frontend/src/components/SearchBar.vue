c06b22000062e4e64512c83a7b278ff889651d0f
이혜은 <snflo16@naver.com>
2021-07-29 15:55:53 +0900

    S05P12C102-106: Update search bar page

---
+++ frontend/src/components/SearchBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div
+      class=" d-flex justify-end align-center"
+    >
+      <v-btn
+        class="ma-1"
+        color="grey"
+        plain
+        @click.stop="searchbar = !searchbar"
+      >
+        Cancel
+      </v-btn>
+    </div>
+    <div style="width: 90%; max-width: 700px;" class="my-4 mx-auto d-flex justify-center align-start">
+      <v-text-field
+        hint="example@naver.com"
+        v-model="search"
+        placeholder="Search User"
+        filled
+        dense
+        rounded
+        pa-0
+        class="mx-3"
+        @keyup.enter="searchUser"
+      ></v-text-field>
+      <button icon color="secondary" class="mt-2 mx-1" @click="searchUser">
+        <v-icon>mdi-magnify</v-icon>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: {
+    search: '',
+    result: []
+  },
+  methods: {
+    searchUser() {
+      this.$store.dispatch('profileStore/findUser', this.search)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
