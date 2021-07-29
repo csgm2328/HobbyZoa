17aaae97daa3d1ee202e6de19b584630e34216ff
박세영 <seyoungs22@naver.com>
2021-07-29 18:12:39 +0900

    S05P12C102-107: update find complete
    S05P12C102-107: Update find complete

--- backend/src/main/java/com/web/curation/controller/FindController.java
+++ backend/src/main/java/com/web/curation/controller/FindController.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.web.curation.find.model.SaveUser;
 import com.web.curation.find.service.FindService;
 import com.web.curation.login.service.JwtService;
+import com.web.curation.response.BasicResponse;
 import com.web.curation.user.model.User;
 
 import io.swagger.annotations.ApiOperation;
@@ -31,21 +34,26 @@ public class FindController {
 	JwtService jwtService;
 
 	@GetMapping(value = "/{nickname}")
-	@ApiOperation(value = "검색할 닉네임", notes = "해당 닉네임 포함하는 유저 모두 검색")
+	@ApiOperation(value = "닉네임 검색", notes = "해당 닉네임 포함하는 유저 모두 검색, 없는 이메일로 찾으면 안됨..")
 	public ResponseEntity<List<Map<String, Object>>> findNickname(@PathVariable("nickname") String findNickname,
-			String email) {
+			String email, HttpServletResponse response) {
 		// 검색어(지금은 닉네임)포함하는 닉네임 모두 리스트에 가져오기
 		List<User> list = findService.findNickname(findNickname);
 		List<Map<String, Object>> nicknameList = new ArrayList<>();
 
-		for (int i = 0; i < list.size(); i++) {
+		if (!list.isEmpty()) {
+			for (int i = 0; i < list.size(); i++) {
+				Map<String, Object> map = new HashMap<>();
+				String nicknames = list.get(i).getNickname();
+				String emails = list.get(i).getEmail();
+				map.put("nickname", nicknames);
+				map.put("email", emails);
+				nicknameList.add(map);
+			}
 
-			Map<String, Object> map = new HashMap<>();
-			String nicknames = list.get(i).getNickname();
-			String emails = list.get(i).getEmail();
-			map.put("nickname", nicknames);
-			map.put("email", emails);
-			nicknameList.add(map);
+		} else {
+			// nicknameList.
+			System.out.println("해당 닉네임 없음");
 		}
 
 		SaveUser saveuser = new SaveUser();
@@ -58,7 +66,7 @@ public class FindController {
 	}
 
 	@GetMapping(value = "/history/{email}")
-	@ApiOperation(notes = "검색 내역 조회", value = "")
+	@ApiOperation(value = "최근 검색 내역 조회", notes = "검색 내역 조회")
 	public ResponseEntity<List<Map<String, Object>>> showHistory(@PathVariable("email") String email) {
 		Map<String, Object> map = new HashMap<>();
 		List<SaveUser> list = findService.showHistory(email);
