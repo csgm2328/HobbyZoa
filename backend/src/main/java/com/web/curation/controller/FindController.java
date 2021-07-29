d77e02dad79c960a96587d0e66b4d8be93708015
박세영 <seyoungs22@naver.com>
2021-07-29 11:09:46 +0900

    Revert "S05P12C102-107: Add find user&history"
    This reverts commit 13cdb3a8fb06b3a5cf99d05cb0a643764a2da9de.

--- backend/src/main/java/com/web/curation/controller/FindController.java
+++ backend/src/main/java/com/web/curation/controller/FindController.java
@@ -5,8 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +14,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.web.curation.find.model.SaveUser;
 import com.web.curation.find.service.FindService;
 import com.web.curation.image.model.Image;
-import com.web.curation.login.service.JwtService;
 import com.web.curation.user.model.User;
 
 import io.swagger.annotations.ApiOperation;
@@ -32,51 +28,25 @@ public class FindController {
 	@Autowired
 	FindService findService;
 	
-	@Autowired
-	JwtService jwtService;
+	@GetMapping(value="/{nickname}") 
+	@ApiOperation(value="검색할 유저", notes="해당 닉네임 포함하는 유저 모두 검색")
+	public ResponseEntity<List<Map<String, Object>>> findNickname(@PathVariable("nickname") String findNickname){
+		Map<String, Object> map = new HashMap<>();	
+		List<User> list = findService.findNickname( findNickname);
+		List<Map<String, Object>> nicknameList =new ArrayList<>();
 
-	@GetMapping(value = "/{nickname}")
-	@ApiOperation(value = "검색할 닉네임", notes = "해당 닉네임 포함하는 유저 모두 검색")
-	public ResponseEntity<List<Map<String, Object>>> findNickname(@PathVariable("nickname") String findNickname,
-			String email) {
-		//검색어(지금은 닉네임)포함하는 닉네임 모두 리스트에 가져오기
-		List<User> list = findService.findNickname(findNickname);
-		List<Map<String, Object>> nicknameList = new ArrayList<>();
-		
 		for (int i = 0; i < list.size(); i++) {
-			Map<String, Object> map = new HashMap<>();
-			String nicknames = list.get(i).getNickname();
-			String emails = list.get(i).getEmail();
-			map.put("nickname", nicknames);
+			String nicknames =  list.get(i).getNickname();
+			String emails =  list.get(i).getEmail();
+			map.put("nickname", nicknames );
 			map.put("email", emails);
 			nicknameList.add(map);
 		}
-		
-		SaveUser saveuser = new SaveUser();
-		saveuser.setSaveNickname(findNickname);
-		saveuser.setSaveEmail(email);
-		// 검색어와 본인 이메일을 find_history에 넣자
-		findService.saveHistory(saveuser);
-		
-		return new ResponseEntity<List<Map<String, Object>>>(nicknameList, HttpStatus.OK);
+		return new ResponseEntity<List<Map<String, Object>>>(nicknameList, HttpStatus.OK); 
 	}
+	
+	
+	
 
-	@GetMapping(value = "/history/{email}")
-	@ApiOperation(notes = "검색 내역 조회", value = "")
-	public ResponseEntity<List<Map<String, Object>>> showHistory(@PathVariable("email") String email) {
-		Map<String, Object> map = new HashMap<>();
-		List<SaveUser> list = findService.showHistory(email);
-		List<Map<String, Object>> historyList = new ArrayList<>();
-
-		for (int i = 0; i < list.size(); i++) {
-			String nicknames = list.get(i).getSaveNickname();
-			String emails = list.get(i).getSaveEmail();
-			map.put("nickname", nicknames);
-			map.put("email", emails);
-			historyList.add(map);
-		}
-
-		return new ResponseEntity<List<Map<String, Object>>>(historyList, HttpStatus.OK);
-	}
-
+	
 }
--- backend/src/main/java/com/web/curation/find/service/FindServiceImpl.java
+++ backend/src/main/java/com/web/curation/find/service/FindServiceImpl.java
@@ -5,37 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.web.curation.feed.model.Feed;
-import com.web.curation.find.model.SaveUser;
 import com.web.curation.find.repo.FindRepo;
-import com.web.curation.find.repo.FindHistoryRepo;
 import com.web.curation.user.model.User;
 
 @Service
 public class FindServiceImpl implements FindService {
-
+	
 	@Autowired
 	FindRepo findRepo;
-
-	@Autowired
-	FindHistoryRepo saveFindRepo;
-
-	@Override
-	public List<User> findNickname(String nickname) {
+	
+	public List<User> findNickname(String nickname){
 		List<User> list = findRepo.findBynicknameContaining(nickname);
 		return list;
-	}
-
-	@Override
-	public List<SaveUser> showHistory(String email) {
-		List<SaveUser> list = saveFindRepo.findBySaveEmail(email);
-		return list;
-	}
-
-	@Override
-	public void saveHistory(SaveUser saveuser) {
-		saveFindRepo.save(saveuser);
-
 	}
 
 }
