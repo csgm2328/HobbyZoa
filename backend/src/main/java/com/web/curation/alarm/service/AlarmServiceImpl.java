02787b5f69b4a97f7b0032325541abb731d2a5a7
추승지 <csgm2328@gmail.com>
2021-08-17 14:45:23 +0900

    refactoring alarm

--- backend/src/main/java/com/web/curation/alarm/service/AlarmServiceImpl.java
+++ backend/src/main/java/com/web/curation/alarm/service/AlarmServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 
 import com.web.curation.alarm.model.Alarm;
-import com.web.curation.alarm.model.Message;
 import com.web.curation.alarm.model.MessageType;
 import com.web.curation.alarm.repo.AlarmRepo;
 
@@ -24,61 +23,44 @@ public class AlarmServiceImpl implements AlarmService{
 	private SimpMessageSendingOperations messagingTemplate;
 	
 	@Override
-	// webSocket: 서비스 알람의 종류에 따라 타겟 유저에 알람보내기
-	// 실은 이렇게 알림을 보내면 알림창을 즉시 띄어주는 거랑 마찬가지다
-	// 하지만 사용자는 알림벨 아이콘을 눌러서 알림을 확인하므로 상호작용이 있다.
-	// 웹소켓으로 쏴줘야하는거는 아이콘에 표시할 알림의 개수다
-	// 이 알림의 개수는 구독하고 있는 연결을 통해 알림이 필요한 서비스후 DB에 자신의 알림 개수의 변화가 있으면 알려준다.
 	public void createAlarm(MessageType alarmType, String from, String to, String content) {
-		if(from.equals(to)) //자신의 피드에 좋아요, 스크랩시 알림 전송 X
+		if(from.equals(to)) 
 			return;
 		Optional<Alarm> e = alarmRepo.findByAlarmTypeAndFromemailAndToemail(alarmType.toString(), from, to); 
-		if(e.isPresent()) {//이미 같은 type, from, to 가같은 알림이 존재한다면 또만들지 않음
+		if(e.isPresent()) { 
 			System.out.println(e.get().getAlarmcode()+ "로 생성된 적 있는 알림");
 			return;
 		}
-		Message msg = new Message();
-		msg.setType(alarmType);
-		msg.setSender(from);
-		msg.setReceiver(to);
-		msg.setContent(content);
 		Alarm alarm = Alarm.builder()
 				.alarmType(alarmType.toString())
 				.fromemail(from)
 				.toemail(to)
 				.content(content)
 				.build();
-		alarmRepo.save(alarm); //DB에 성공적으로 저장되면 안읽은 알림 개수 변동을 알림
+		alarmRepo.save(alarm); 
 		messagingTemplate.convertAndSend("/queue/" + to, alarmRepo.countByToemailAndAlarmCheck(to, false));
-//		messagingTemplate.convertAndSend("/queue/" + to, msg);
 	}
 
-	//한달 내 생성된 알람 리스트 반환
-	//읽은거는 읽은 날짜 순으로 order by check_date desc
-	//안읽은거는 생성순으로 order by create_date desc
-	//안읽은거부터 합쳐서 리스트반환
 	@Override
 	public List<Alarm> findAll(String email) {
 		List<Alarm> total = new ArrayList<Alarm>();
 		List<Alarm> alreadyCheckedList = alarmRepo.findAllByToemailAndAlarmCheckOrderByCheckDateDesc(email, true);
 		List<Alarm> unCheckedList = alarmRepo.findAllByToemailAndAlarmCheckOrderByCreateDateDesc(email, false);
-		for(Alarm x: unCheckedList) // 확인안한 알림이 위로 오도록
+		for(Alarm x: unCheckedList) 
 			total.add(x);
 		for(Alarm x: alreadyCheckedList) {
-			LocalDateTime monthago = LocalDateTime.now().minusMonths(1); //확인 한거는 한달 내 생성된거만 보여주기
+			LocalDateTime monthago = LocalDateTime.now().minusMonths(1); 
 			if(x.getCreateDate().isAfter(monthago))
 				total.add(x);
 		}
 		return total;
 	}
-	//읽은 알림처리하고 읽은시간 생성
 	@Override
 	public Alarm CheckAlarm(int code) {
 		Alarm alarm = alarmRepo.findByAlarmcode(code);
 		System.out.println("[ " + code + " ]번 알림 읽음");
 		alarm.setAlarmCheck(true);
-		alarm.setCheckDate(ZonedDateTime.now(ZoneId.of("Asia/Seoul")).toLocalDateTime()); //읽은시간 생성
-		//읽은 후에 다시 소켓에 안읽은 알림 수 리턴
+		alarm.setCheckDate(ZonedDateTime.now(ZoneId.of("Asia/Seoul")).toLocalDateTime()); 
 		alarm = alarmRepo.save(alarm);
 		messagingTemplate.convertAndSend("/queue/" + alarm.getToemail(), alarmRepo.countByToemailAndAlarmCheck(alarm.getToemail(), false));
 		return alarm;
--- backend/src/main/java/com/web/curation/config/WebSocketConfig.java
+++ backend/src/main/java/com/web/curation/config/WebSocketConfig.java
@@ -6,24 +6,22 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-import com.web.curation.alarm.service.WebSocketHandler;
-
 @Configuration
 @EnableWebSocketMessageBroker //WebSocket 서버를 가능하게 하는 어노테이션
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{ //서버 활성화 interface
-	WebSocketHandler webSocketHandler;
 	//클라이언트에서 클라이언트로 메시지 라우팅하기 위한 메시지 브로커 구성
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableSimpleBroker("/topic", "/queue");
-//        webSocketHandler.handleMessage(session, message);
     }
     //서버 연결을 위한 엔드포인트 구성
 	@Override
     public void registerStompEndpoints(StompEndpointRegistry registry) { 
-		//STOMP: 단순 메세지 프로토콜이다. WebSocket은 그냥 통신일 뿐이라 특정 주제나 사용자에게 메시지를 보내는 방법을 정의하기 위함
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS(); //withSockJs: 웹소켓 지원하지 않는 브라우저에 폴백 옵션 제공
+		//STOMP: 단순 메세지 프로토콜이다.
+		//WebSocket은 그냥 통신일 뿐이라 특정 주제나 사용자에게 메시지를 보내는 방법을 정의하기 위함
+		//withSockJs: 웹소켓 지원하지 않는 브라우저에 폴백 옵션 제공
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS(); 
     }
 
 	
